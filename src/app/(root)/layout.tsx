37f227fdad628f1aafb4b8987632a35a0d4dd2c8
gauss2302 <shilov6865@gmail.com>
2024-05-13 13:59:51 +0500

    Completed Menu Navigaton Bar for Desktop and Mobile Devices

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,14 +1,32 @@
 import React from "react";
+import Home from "@/app/(root)/page";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNavBar from "@/components/MobileNavBar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com",
+  };
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNavBar user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 export default function Home() {
+  const loggedIn = { firstName: "John Doe" };
   return (
-    <main>
-      <div>Home</div>
-    </main>
+    <section className={"home"}>
+      <div className={"home-content"}>
+        <header className={"home-header"}>
+          <HeaderBox
+            type={"greeting"}
+            title={"Welcome"}
+            user={loggedIn?.firstName || "Guest"}
+            subtext={"Manage your account wisely"}
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1500.22}
+          />
+        </header>
+      </div>
+    </section>
   );
 }
